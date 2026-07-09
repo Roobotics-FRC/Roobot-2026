d9f78dfd3fa5e4b076967978fdcb5799607421f9
mia <211606073+miabugy@users.noreply.github.com>
2026-07-09 14:02:26 -0400

    fixed coasting?

--- src/main/java/frc/robot/subsystems/ShootSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShootSubsystem.java
@@ -1,10 +1,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.InvertedValue;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-import com.revrobotics.spark.SparkMax;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -14,14 +14,19 @@ public class ShootSubsystem extends SubsystemBase {
     private final TalonFX intakeMotor = new TalonFX(21); 
     private final TalonFX shootMotor = new TalonFX(23);
 
-    public ShootSubsystem(){
-        TalonFXConfiguration intakeConfig = new TalonFXConfiguration();
-
-        intakeConfig.MotorOutput
-            .withInverted(InvertedValue.Clockwise_Positive);
+    public NeutralModeValue brake = NeutralModeValue.Brake;
     
-        intakeMotor.getConfigurator().apply(intakeConfig);
-        shootMotor.getConfigurator().apply(intakeConfig);
+    public ShootSubsystem(){
+        TalonFXConfiguration shootConfig = new TalonFXConfiguration()
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withNeutralMode(NeutralModeValue.Brake)
+            );
+
+        shootConfig.MotorOutput.withInverted(InvertedValue.Clockwise_Positive);
+
+        intakeMotor.getConfigurator().apply(shootConfig);
+        shootMotor.getConfigurator().apply(shootConfig);
     }
     
     public void SetShootSpeed(double speed) {
